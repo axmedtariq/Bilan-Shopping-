c85f97a15b932004a604050d28afca9df35818b3
unknown <axmedobsiye15@gmail.com>
2022-08-08 08:34:52 +0300

    first Homepage part start

---
+++ src/Homepage.component.js
@@ -0,0 +1,43 @@
+import React from 'react';
+const Homepage = ()  => (
+    <div className='Homepage'>
+        <div className='Directory-Menu'>
+            <div className='Menu-Item'>
+                <div className='content'>
+                    <h1 className='title'>HATS </h1>
+                    <span className='subtitle'>SHOP NOW </span>
+
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS  </h1>
+                    <span className='subtitle'>SHOP NOW </span>
+
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS  </h1>
+                    <span className='subtitle'>SHOP NOW </span>
+
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS </h1>
+                    <span className='subtitle'>SHOP NOW </span>
+
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='content'>
+                    <h1 className='title'>MEN'S  </h1>
+                    <span className='subtitle'>SHOP NOW </span>
+
+                </div>
+            </div>
+        </div>
+    </div>
+);
+export default Homepage;
